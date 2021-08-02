44ecd1ffc946a1162bec6429b9a6ba95ae1d186f
Mariano Crosetti <marianocrosetti1993@gmail.com>
2021-08-02 20:00:15 -0300

    Add validation on cellphone

--- android-app/FastAIDemo/app/src/main/java/com/example/fastaidemo/MainActivity.java
+++ android-app/FastAIDemo/app/src/main/java/com/example/fastaidemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fastaidemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.Manifest;
 import android.content.Context;
@@ -53,6 +54,11 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
 
+    // Flag for enable/disable running validation when launching the app
+    final boolean RUN_VALIDATION = true;
+    // Validation percentage of image used (for >10 the process is very time consuming)
+    final int RUN_VALIDATION_PCT = 1;
+
     // Internal request codes that are lunched by the app
     // Request for storage permissions
     final int REQUEST_GALLERY_PERMISSIONS = 1;
@@ -86,6 +92,35 @@ public class MainActivity extends AppCompatActivity {
                     TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB, MemoryFormat.CHANNELS_LAST);
             System.out.println("inputTensor[100,200,0] = " + tensor.getDataAsFloatArray()[0+100*3+200*3*720]);
             System.out.println("inputTensor[44,123,2] = " + tensor.getDataAsFloatArray()[2+44*3+123*3*720]);
+
+            if (RUN_VALIDATION) {
+                String[] validation_fns = getAssets().list("validation/");
+                System.out.println("Running validation set from: " + validation_fns.length);
+                int processedImages = 0, processedCatImages = 0, processedDogImages = 0, isCorrectPrediction=0;
+                for (int i = 0; i < validation_fns.length ; ++i)
+                    if (i%100 < RUN_VALIDATION_PCT) {
+                        String assetFn = "validation/"+validation_fns[i];
+                        InputStream istr = getAssets().open(assetFn);
+                        Bitmap bitmap = BitmapFactory.decodeStream(istr);
+                        istr.close();
+                        if (bitmap!=null) {
+                            float[] scores = predictionCatOrDog(bitmap, false);
+                            int predictedClass = argMax(scores);
+
+                            /*if (i%100==0)*/
+                            System.out.println(validation_fns[i]);
+                            System.out.println(Character.isUpperCase(validation_fns[i].charAt(0)) ? "cat" : "dog");
+                            int realClass = Character.isUpperCase(validation_fns[i].charAt(0)) ? 0 : 1;
+                            System.out.println(predictedClass + " " + realClass);
+                            if ( ++processedImages % 100 == 0 ) System.out.println(processedImages);
+                            if (realClass==0) ++processedDogImages; else ++processedCatImages;
+                            isCorrectPrediction += (predictedClass==realClass) ? 1 : 0;
+                         }
+                }
+                System.out.println(String.format("Cats: %d (%.2f) Dogs: %d (%.2f) Acc: %.8f",processedCatImages,((float)processedCatImages)/processedImages,
+                        processedDogImages,((float)processedDogImages)/processedImages,((float)isCorrectPrediction)/processedImages));
+            }
+
         } catch (IOException e) {
             Log.e("FastAIDemo", "Error reading assets", e);
             finish();
@@ -120,33 +155,43 @@ public class MainActivity extends AppCompatActivity {
 
             // Code above was (almost) all copied (https://www.viralpatel.net/pick-image-from-galary-android-app/ thanks)
             // This call the inference method and show the results on the text view
-            textView.setText(showPredictionCatOrDog(bitmap));
+            final float[] scores = predictionCatOrDog(bitmap, true);
+            int maxScoreIdx = argMax(scores);
+            System.out.println("maxScoreIdx: " + maxScoreIdx);
+
+            String[] CLASSES = new String[]{ "cat", "dog" };
+            System.out.println("Predicted class: " + CLASSES[maxScoreIdx]);
+
+            textView.setText(String.format("Prediction: %s (%.2f %% cat | %.2f %% dog)", CLASSES[maxScoreIdx], scores[0]*100, scores[1]*100));
         }
     }
 
-    // Here we do all the pytorch stuff (except with loading the model, done in onCreate) and produce the text output
-    private String showPredictionCatOrDog(Bitmap bitmap) {
+    // Here we do all the pytorch stuff (except with loading the model, done in onCreate) and return the predicted class probabilities
+    private float[] predictionCatOrDog(Bitmap bitmap, boolean verbose) {
         // Convert bitmap to Float32 tensor is equivalent to:
         // - Load the image (pixels as 0 to 255 bytes).
         // - Apply torchvision.transforms.ToTensor, scaleing values from 0 to 1 (dividing by 255).
         // - Apply transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-        bitmap = Bitmap.createScaledBitmap(bitmap,224,224, true);
+        bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
         final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
                 TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB, MemoryFormat.CHANNELS_LAST);
 
-        System.out.println("Shape: " + Arrays.toString(inputTensor.shape()));
+        if (verbose) System.out.println("Shape: " + Arrays.toString(inputTensor.shape()));
 
         // Forward pass, run the model
         // We do not resize to 224 x 224 thanks to AdaptiveAvgPool2d (but it could be a good idea to speed up inference process)
         // In production this SHOULD NOT be done in the main thread because is a lot of work and will block the app
-        System.out.println("Forward begin");
+        if (verbose) System.out.println("Forward begin");
         final Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
-        System.out.println("Forward ends");
+        if (verbose) System.out.println("Forward ends");
 
         // Getting tensor content as java array of floats
         final float[] scores = outputTensor.getDataAsFloatArray();
-        System.out.println("scores: " + Arrays.toString(scores));
+        if (verbose) System.out.println("scores: " + Arrays.toString(scores));
+        return scores;
+    }
 
+    private int argMax(float[] scores) {
         // Searching for the index with maximum score
         int maxScoreIdx = 0;
         for (int i = 0; i < scores.length; i++) {
@@ -154,26 +199,7 @@ public class MainActivity extends AppCompatActivity {
                 maxScoreIdx = i;
             }
         }
-
-        String[] CLASSES = new String[]{ "cat", "dog" };
-        System.out.println("maxScoreIdx: " + maxScoreIdx);
-        System.out.println("Predicted class: " + CLASSES[maxScoreIdx]);
-
-        return String.format("Prediction: %s (%.2f %% cat | %.2f %% dog)", CLASSES[maxScoreIdx], scores[0]*100, scores[1]*100);
-    }
-
-    //
-    public static Bitmap scaleDown(Bitmap realImage, float maxImageSize,
-                                   boolean filter) {
-        float ratio = Math.min(
-                (float) maxImageSize / realImage.getWidth(),
-                (float) maxImageSize / realImage.getHeight());
-        int width = Math.round((float) ratio * realImage.getWidth());
-        int height = Math.round((float) ratio * realImage.getHeight());
-
-        Bitmap newBitmap = Bitmap.createScaledBitmap(realImage, width,
-                height, filter);
-        return newBitmap;
+        return maxScoreIdx;
     }
 
     // This function was copied from HelloWorldApp (android-demo-app from pytorch's repo)
